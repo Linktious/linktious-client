3b23a4376b8ca7868d19aa4a4a2ea2f8cb417037
Asaf <asafsemail@gmail.com>
2021-06-05 18:40:35 +0300

    Style link component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Route, BrowserRouter, Switch } from 'react-router-dom'
 import { Provider as ReduxProvider } from 'react-redux'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
-import { ThemeProvider } from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
 import store from '~/store'
 import { useAppDispatch } from '~/store/hooks'
 import theme, { GlobalStyle } from '~/Theme'
@@ -12,6 +12,19 @@ import Board, { fetchBoards } from '~/features/boards'
 import Link, { fetchLinks } from '~/features/links'
 import { fetchLabels } from '~/features/labels'
 
+
+const Root = styled.div`
+  height: 100%;
+  width: 100%;
+  flex: 1;
+  display: flex;
+`
+
+const Content = styled.div`
+  flex: 1;
+  display: flex;
+  margin: 16px;
+`
 
 const App = () => {
   const dispatch = useAppDispatch()
@@ -42,27 +55,32 @@ const App = () => {
   }
 
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/boards" render={() => <Board boardId={1}/>}/>
-        <Route path="/link" render={() => <Link linkId={1}/>}/>
-        <Route path="/" render={() => <div>render</div>}/>
-      </Switch>
-    </BrowserRouter>
+    <Content>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/board" render={() => <Board boardId={1}/>}/>
+          <Route path="/link" render={() => <Link linkId={1}/>}/>
+          <Route path="/" render={() => <div>render</div>}/>
+        </Switch>
+      </BrowserRouter>
+    </Content>
   )
 }
 
 
 const ProvidedApp = () => (
-  <MuiThemeProvider theme={theme}>
-    <CssBaseline/>
-    <ThemeProvider theme={theme}>
-      <ReduxProvider store={store}>
-        <GlobalStyle/>
-        <App/>
-      </ReduxProvider>
-    </ThemeProvider>
-  </MuiThemeProvider>
+  <Root>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline/>
+      <ThemeProvider theme={theme}>
+        <ReduxProvider store={store}>
+          <GlobalStyle/>
+          <App/>
+        </ReduxProvider>
+      </ThemeProvider>
+    </MuiThemeProvider>
+  </Root>
+
 )
 
 
--- src/features/boards/Board.tsx
+++ src/features/boards/Board.tsx
@@ -3,29 +3,63 @@ import { useAppSelector } from '~/store/hooks'
 import { selectBoardById } from './slice'
 import { selectLinksByLabels } from '~/features/links/slice'
 import Link from '~/features/links'
+import styled from 'styled-components'
 
 interface BoardProps {
   boardId: number
 }
 
+const Root = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-content: flex-start;
+`
+
+const Title = styled.div`
+  font-size: xxx-large;
+`
+
+const LinksContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  align-content: flex-start;
+  justify-content: flex-start;
+`
+
+const LinkContainer = styled.div`
+  margin: 16px;
+  height: fit-content;
+  width: fit-content;
+`
+
 const Board = (props: BoardProps) => {
   const { boardId } = props
   // const dispatch = useAppDispatch()
   const board = useAppSelector(selectBoardById(boardId))
-  const links = useAppSelector(selectLinksByLabels(board ? board.labelsFilters : []))
+  if (!board) return null
+
+  const links = useAppSelector(selectLinksByLabels(board.labelsFilters))
   console.log(links)
 
   return (
-    <div>
-      {
-        links.map((link) => (
-          <Link
-            key={`link-${link.id}`}
-            linkId={link.id}
-          />
-        ))
-      }
-    </div>
+    <Root>
+      <Title>Tutorials</Title>
+      <LinksContainer>
+        {
+          links.map((link) => (
+            <LinkContainer
+              key={`link-${link.id}`}
+            >
+              <Link
+                linkId={link.id}
+              />
+            </LinkContainer>
+          ))
+        }
+      </LinksContainer>
+    </Root>
   )
 }
 
